b7aac1c61cedc5fd629419d69ddca55788b158ac
MdSahilAlii <aksahil18@gmail.com>
2025-05-01 15:48:53 +0530

    tech statcks added

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -106,15 +106,26 @@ const techStacks = [
   { icon: "css.svg", language: "CSS" },
   { icon: "javascript.svg", language: "JavaScript" },
   { icon: "tailwind.svg", language: "Tailwind CSS" },
-  { icon: "reactjs.svg", language: "ReactJS" },
-  { icon: "vite.svg", language: "Vite" },
+  { icon: "C.svg", language: "C" },
   { icon: "nodejs.svg", language: "Node JS" },
+  {icon:"MongoDB.svg", language:"MongoDB"},
+  {icon: "Express.svg", language:"Express.js"},
+  { icon: "reactjs.svg", language: "ReactJS" },
+  {icon:"Next.js.svg",language:"NextJS"},
+  { icon: "vite.svg", language: "Vite" },
+  
+  
   { icon: "bootstrap.svg", language: "Bootstrap" },
   { icon: "firebase.svg", language: "Firebase" },
+  {icon:"Vue.js.svg",language:"VueJS"},
   { icon: "MUI.svg", language: "Material UI" },
   { icon: "vercel.svg", language: "Vercel" },
+  { icon: "Figma.svg", language: "Figma" },
   { icon: "SweetAlert.svg", language: "SweetAlert2" },
-  // {icon: "Nextjs.svg", language: "Next.js"},
+  
+  
+  
+
 ];
 
 export default function FullWidthTabs() {
