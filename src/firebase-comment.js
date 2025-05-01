d339b6c8ed61ec2bd99bbc38c16e8b03e0b54d1c
MdSahilAlii <aksahil18@gmail.com>
2025-05-02 00:12:09 +0530

    certificate added

--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -4,15 +4,27 @@ import { getStorage } from "firebase/storage";
 import { collection, addDoc } from "@firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//phle yeh tha
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCL3G1AYsmqjj0ksRqBxZeSl9i5X6IXLF0",
+//     authDomain: "portfolio-website-f587e.firebaseapp.com",
+//     projectId: "portfolio-website-f587e",
+//     storageBucket: "portfolio-website-f587e.firebasestorage.app",
+//     messagingSenderId: "471461018678",
+//     appId: "1:471461018678:web:44262c1a407b1f6884ec97",
+//     measurementId: "G-9YM110GJX6"
+//   };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCL3G1AYsmqjj0ksRqBxZeSl9i5X6IXLF0",
-    authDomain: "portfolio-website-f587e.firebaseapp.com",
-    projectId: "portfolio-website-f587e",
-    storageBucket: "portfolio-website-f587e.firebasestorage.app",
-    messagingSenderId: "471461018678",
-    appId: "1:471461018678:web:44262c1a407b1f6884ec97",
-    measurementId: "G-9YM110GJX6"
-  };
+  apiKey: "AIzaSyB5RTprSnjuJZ8tlfvDajF0fxa6qNccowA",
+  authDomain: "final-portfilio.firebaseapp.com",
+  projectId: "final-portfilio",
+  storageBucket: "final-portfilio.firebasestorage.app",
+  messagingSenderId: "161590000849",
+  appId: "1:161590000849:web:e37c06de9778fdce5099f0",
+  measurementId: "G-YCW9QM187G"
+};
 
 // Initialize with a unique name
 const app = initializeApp(firebaseConfig, 'comments-app');
